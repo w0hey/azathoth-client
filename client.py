5289f759959b12abf7b4a3058c0b786983e231e1
Steve McGrath <smcgrath23@gmail.com>
2012-05-24 20:45:38 -0500

    tweaks to launcher

--- client.py
+++ client.py
@@ -6,12 +6,15 @@ from twisted.internet import gtk2reactor
 gtk2reactor.install()
 
 from twisted.internet import reactor
-
+from twisted.python import log
 from view import MainView
 from controller import MainController
 from model import MainModel
 
 if __name__ == "__main__":
+    observer = log.PythonLoggingObserver()
+    observer.start()
+    #logging.getLogger('gtkmvc').setLevel(logging.DEBUG)
     m = MainModel()
     v = MainView()
     c = MainController(m, v)
