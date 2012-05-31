9ca9ef56def7b34f1ee400f0e99c6358fe74a383
Steve McGrath <smcgrath23@gmail.com>
2012-05-31 18:07:41 -0500

    implement periodic resets of the safety timer

--- models/drivemodel.py
+++ models/drivemodel.py
@@ -1,6 +1,7 @@
 import logging
 
 from gtkmvc import Model
+from twisted.internet import task
 
 class DriveModel(Model):
     estop_act = "N/A"
@@ -19,7 +20,7 @@ class DriveModel(Model):
     cal_eeprom_x = 0
     cal_eeprom_y = 0
     deadman = True
-    counting = True
+    counting = False
     looper = None
 
     __observables__ = ("estop_act", "estop_cmd", "select_act", "select_cmd", "moving",
@@ -33,19 +34,26 @@ class DriveModel(Model):
         self.factory = self.parent.factory
 
     def joystickCommand(self, x, y):
-        #if not self.counting and (x | y != 0):
-        #    self.looper = task.LoopingCall(self.joystickCommand, (x, y))
-        #    self.looper.start(0.2) # Run every 200ms
+        if self.counting == False and (x | y != 0):
+            self.looper = task.LoopingCall(self.resetTimer)
+            self.looper.start(0.2, now=False) # Run every 200ms
+            self.counting = True
         self.factory.control.send_joystick_command(x, y)
         self.parent.joy_x = x
         self.parent.joy_y = y
-        #if self.counting:
-        #    self.looper.reset()
-        #    if (x | y == 0):
-        #        self.looper.stop()
+        if self.counting == True:
+            self.looper.reset()
+            if (x | y == 0):
+                self.looper.stop()
+                self.counting = False
+
+    def resetTimer(self):
+        # Resend the last joystick position to reset
+        # the drive controller's safety timer
+        self.factory.control.send_joystick_command(self.parent.joy_x, self.parent.joy_y)
 
     def onStatusUpdate(self, status, xpos, ypos, xval, yval):
-        logging.debug('status %x' % status)
+        #logging.debug('status %x' % status)
         self.estop_act = 'RUN' if (status & 0x01 == 0x01) else 'STOP'
         self.estop_cmd = 'STOP' if (status & 0x02 == 0x02) else 'RUN'
         self.select_act = 'ROBOT' if (status & 0x04 == 0x04) else 'CHAIR'
